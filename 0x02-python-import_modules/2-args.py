0c9ab12db3f3e9e3c344307dd268104ab2e55885
hamza <elassri2000@gmail.com>
2024-03-25 12:04:05 +0000

    edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-if len(argv) == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-sum = 0
+    sum = 0
 
-for i in range(1, len(argv)):
-    sum = sum + int(argv[i])
-print(sum)
+    for i in range(1, len(argv)):
+        sum = sum + int(argv[i])
+    print(sum)
